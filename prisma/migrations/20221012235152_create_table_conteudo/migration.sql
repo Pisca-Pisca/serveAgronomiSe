501011433a395760bf71c7f971618463200fde85
Nickcarv18 <nicolicarvalho1@hotmail.com>
2022-10-12 20:54:54 -0300

    First Commit

---
+++ prisma/migrations/20221012235152_create_table_conteudo/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "conteudo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "titulo" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "imagem" BLOB NOT NULL
+);
