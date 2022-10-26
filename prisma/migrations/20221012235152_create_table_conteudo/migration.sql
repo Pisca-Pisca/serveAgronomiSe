d295be2973d6b7ef2355e9b9729914a40d88e37e
Nickcarv18 <nicolicarvalho1@hotmail.com>
2022-10-25 21:11:31 -0300

    Mudando tecnologia de back-end e BD

--- prisma/migrations/20221012235152_create_table_conteudo/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "conteudo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "titulo" TEXT NOT NULL,
-    "descricao" TEXT NOT NULL,
-    "link" TEXT NOT NULL,
-    "imagem" BLOB NOT NULL
-);
