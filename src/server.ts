52bb52d296ea0af4cd456ae07e32ee9d1bc5bb97
Nickcarv18 <nicolicarvalho1@hotmail.com>
2022-11-04 15:32:54 -0300

    Configurando procfile

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import {router} from "./src/routes";
+import {router} from "./routes";
 
 const app = express();
 const port = process.env.PORT || 3333;
