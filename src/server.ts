21c6c663b21cfbe5580d79367bcc0ad042d0b682
Nickcarv18 <nicolicarvalho1@hotmail.com>
2022-11-04 15:06:58 -0300

    Ajuste na porta do listen

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import cors from 'cors';
 import {router} from "./routes";
 
 const app = express();
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(3333);
+app.listen(port);
